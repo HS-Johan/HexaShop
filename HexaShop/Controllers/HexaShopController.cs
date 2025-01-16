c86b5f7555a3216db62bcfd71aabd850c2d9a9f8
Hasan Shahriar <johan.cse5.bu@gmail.com>
2025-01-16 07:04:24 +0600

    Temp_16Jan24_v1

---
+++ HexaShop/Controllers/HexaShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HexaShop.Controllers
+{
+    public class HexaShopController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
